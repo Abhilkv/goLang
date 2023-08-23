b5d4e245560f2e334319a3ae6e6081b09afa3ca8
Abhilkv <abhilknight@gmail.com>
2023-08-23 08:09:08 +0530

    first golang code

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nameOne string = "ABHIL"
+
+	var nameThree string // declaring the variable first and initialising it later
+	nameThree = "Sobhana"
+
+	var nameTwo = "K V" // type will be asigned dynamically
+
+	nameFour := "Ahul" //  short hand of writing without var , can't be used outside of functions
+
+	fmt.Println("test", nameOne, nameTwo, nameThree, nameFour)
+}
