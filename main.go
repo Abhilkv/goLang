cffa41b4e9056b6e413747a6a701b4b515a4675d
Abhil Vidhyadharan <Abhil.Vidhyadharan@StoneX.com>
2023-09-02 10:04:52 +0530

    functions and function returing

--- main.go
+++ main.go
@@ -3,10 +3,30 @@
 
 package main
 
-import "fmt"
+import ("fmt"
+"strings"
+)
+func userMessage(users []string, printer func(string, string)) {
+    for _, value := range users { // Use _ to ignore the index
+        printer(value, " Welcome") // Add a space before "Welcome"
+    }
+}
 
-func variables() {
-       fmt.Print("\n----------------     VARIABLES    ---------------- \n ")
+func greeting(user string, message string) {
+    fmt.Println(user + message) // Use + to concatenate strings
+}
+
+func welcome() (string, string)(string, int) {
+    users := []string{"ABHIL", "AKHIL", "BINIL", "ROHIT"}
+    userMessage(users, greeting)
+    return "SUCCESS", 10;
+}
+
+
+func variablesAndFunctions() {
+     fmt.Print("\n----------------     VARIABLES    ---------------- \n ")
+     operationStatus, operationValue := welcome()
+     fmt.Println("value after calling functions", operationStatus, operationValue)
     // strings
     var nameOne string = "ABHIL"
 
@@ -56,6 +76,7 @@ func printing() {
 }
 
 func arrayAndSlice() {
+    fmt.Print("\n----------------     ARRAY AND SLICES   ----------------  \n ")
     var ages [3]int = [3]int{20, 30, 25}
     var age2 [2]int = [2]int{30}
     age3 := [2]string{"abhil", "kv"}
@@ -72,14 +93,91 @@ func arrayAndSlice() {
     test := scores[1:2]
     test2 := scores[2:]
     test3 := scores[:3]
+    fmt.Println(test, test2, test3)
+}
+
+func sliceMethods() {
+    fmt.Print("\n----------------     SLICE METHODS   ----------------  \n ")
+    makeSlice := make([]int, 5, 10) // Creates a slice with length 5 and capacity 10
+
+    mySlice := []int{1, 2, 3}
+    appendData := append(mySlice, 4, 5) // "appendData" variable was missing
+
+    source := []int{1, 2, 3}
+    copyDestination := make([]int, len(source))
+    copy(copyDestination, source) // Corrected variable name and added "copy" function
+
+    original := []int{1, 2, 3, 4, 5}
+    subSlice := original[1:4]
+    length := len(original)
+
+    fmt.Println(makeSlice, appendData, copyDestination, subSlice, length)
+}
+
+func stringMethods() {
+fmt.Print("\n----------------     STRING METHODS   ----------------  \n ")
+    s := "Hello, world!   "
+    length := len(s)
+    hasPrefix := strings.HasPrefix(s, "Hello")
+    hasSuffix := strings.HasSuffix(s, "world")
+    contains := strings.Contains(s, "world") // true / false
+    index := strings.Index(s, "world")       // -1 if not found
+    lastOccurrence := strings.LastIndex(s, "l") // returns 10, Returns -1 if not found
+    replace := strings.Replace(s, "!", " Abhil", 1) // last is count -> how many times need to be replaced
+    tolower := strings.ToLower("HELLO")
+    toupper := strings.ToUpper("world")
+    trim := strings.TrimSpace(s)          // removes white space
+
+    split := strings.Split(s, ",")       // []string{"Hello", " world!   "}
+    join := strings.Join(split, ", ")
+
+    fmt.Println(length, hasPrefix, hasSuffix, contains, index, lastOccurrence, replace, tolower, toupper, trim, split, join)
     
+}
+
+func loops() {
+    fmt.Print("\n----------------     LOOPS   ----------------  \n ")
+    for i := 0; i < 5; i++ {
+        fmt.Println(i)
+    }
     
+    fmt.Println("------------")
+    i := 6
+    for i < 10 {
+        
+        if i == 8 {
+            break
+        }
+        // if i == 7 {
+        //     continue
+        // }
+        fmt.Println(i)
+        i++
+    }
+    fmt.Println("------------")
     
+    numbers := []int{11,22,33,44,55,66,77,88,99}
+    for index, value := range numbers {
+        fmt.Println(index, value)
+    }
+    
+    fmt.Println("------------")
+    // goto
+    j := 10
+    loopLable:
+    if j < 15 {
+        fmt.Println(j)
+        j++
+        goto loopLable
+    }
     
 }
 
 func main() {
     printing()
-    variables()
+    variablesAndFunctions()
     arrayAndSlice()
+    sliceMethods()
+    stringMethods()
+    loops()
 }
